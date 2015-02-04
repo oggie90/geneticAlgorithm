7143593e77a604e6d0ef3904935ede388b0a4062
oggie90 <jogden16761@gmail.com>
2015-02-04 21:55:48 +0000

    still building

--- geneticAlgorithm/Genetics.h
+++ geneticAlgorithm/Genetics.h
@@ -1,20 +1,25 @@
 #include <boost\dynamic_bitset.hpp>
 
 using namespace boost;
+using geneCode = dynamic_bitset < > ;
+
 
 class Genetics
 {
 public:
 	Genetics();
-	Genetics(dynamic_bitset<> genes){};
+	Genetics(dynamic_bitset<> genes, int outputSize){};
 	~Genetics();
 
-	int start();
+	int run();
 
 
 
 private:
+	geneCode genes;
+	int outPutSize;
 	
+	int processBits(geneCode input);
 
 
 
